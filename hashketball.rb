74d2a6564628a71d4c6734301c855dbbfdb63ef8
Alexander Truong <alexander.m.truong@gmail.com>
2018-10-01 18:47:31 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -167,6 +167,7 @@ end
 def team_names
   teams = []
  game_hash.each do |location, team|
+   team.each do |team_data, data|
    binding.pry
    if team == :team_name
      teams << team[:team_name]
